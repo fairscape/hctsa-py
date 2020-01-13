bb622e8d08f4ed17e87237d8ae56d9e4530f6886
jniestroy <jniestroy@gmail.com>
2020-01-13 15:15:35 -0500

    MF_ARMA_orders

---
+++ Operations/MF_ARMA_orders.py
@@ -0,0 +1,35 @@
+def MF_ARMA_orders(y,pr = [1,2,3,4,5],qr=[0,1,2,3,4,5]):
+
+    aics = np.zeros((len(pr),len(qr)))
+    bics = np.zeros((len(pr),len(qr)))
+
+    for i in range(len(pr)):
+
+        for j in range(len(qr)):
+
+            p = pr[i]
+            q = qr[i]
+
+            model = ARIMA(y, order=(p,0,q))
+            model_fit = model.fit( disp=False)
+
+            aics[i,j] = model_fit.aic
+            bics[i,j] = model_fit.bic
+
+    outDict = {}
+
+    outDict['aic_min'] = np.min(aics)
+
+    mins = np.argwhere(aics == np.min(aics))[0]
+
+    outDict['opt_p'] = pr[mins[0]]
+
+    outDict['opt_q'] = qr[mins[0]]
+
+    outDict['meanAICS'] = np.mean(aics)
+    outDict['stdAICS'] = np.std(aics)
+
+    outDict['meanBICS'] = np.mean(bics)
+    outDict['stdBICS'] = np.std(bics)
+
+    return outDict
