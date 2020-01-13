65bf1cc7ffd651a968b099706c6c8e8e33b0fb9e
jniestroy <jniestroy@gmail.com>
2020-01-13 15:47:11 -0500

    Finished MF_GARCHfit

--- Operations/MF_GARCHFit.py
+++ Operations/MF_GARCHFit.py
@@ -1,2 +1,38 @@
-def MF_GARCHFit(y,preproc,P,Q):
-    pass
+import arch
+from pprint import pprint
+
+from statsmodels.stats.diagnostic import het_arch
+
+def MF_GARCHFit(y,preproc = None,P = 1,Q = 1):
+
+    y = (y - np.mean(y)) / np.std(y)
+
+    N = len(y)
+
+    outDict = {}
+
+    lm, lmpval,fval,fpval = het_arch(y)
+
+    outDict['lm'] = lm
+    outDict['lmpval'] = lmpval
+    outDict['fval'] = fval
+    outDict['fpval'] = fpval
+
+    model= arch.arch_model(y, vol='Garch', p=P, o=0, q=Q, dist='Normal')
+    results=model.fit()
+
+    #print(results.summary())
+
+    params = results._params
+    paraNames = results._names
+
+    outDict['logl'] = results._loglikelihood
+    outDict['success'] = results._optim_output['success']
+
+    for i in range(len(params)):
+
+        outDict[paraNames[i]] = params[i]
+
+    #pprint(vars(results))
+
+    return outDict
