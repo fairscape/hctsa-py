f378ec2c2503643598dfb1f4b5d96e0c3e34d275
jniestroy <jniestroy@gmail.com>
2020-01-14 13:43:07 -0500

    Fixed rdiv

--- PeripheryFunctions/ZG_rdiv.py
+++ PeripheryFunctions/ZG_rdiv.py
@@ -1,13 +1,25 @@
 def ZG_rdiv(X,Y):
 
     if X.shape[0] != Y.shape[0] or len(Y.shape) != 1:
-
-        return None
+        if len(Y.shape) > 1:
+            if Y.shape[1] != 1:
+                    print('Error')
+                    return None
+        else:
+            print('Error')
+            return None
 
     Z = np.zeros(X.shape)
 
+    if len(X.shape) == 1:
+
+        for i in range(X.shape[0]):
+            Z[i] = X[i] / Y[i]
+        
+        return Z
+
     for i in range(X.shape[1]):
 
-        Z[:,i] = np.divide(X[:,i],Y)
+        Z[:,i] = np.divide(X[:,i],Y.flatten())
 
     return Z
