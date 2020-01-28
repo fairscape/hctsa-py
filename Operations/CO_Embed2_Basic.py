c0a83cb4818978f08e25b940c67188b798b5d20f
jniestroy <jniestroy@gmail.com>
2020-01-28 10:05:13 -0500

    Added all outputs to match matlab

--- Operations/CO_Embed2_Basic.py
+++ Operations/CO_Embed2_Basic.py
@@ -24,20 +24,51 @@ def CO_Embed2_Basic(y,tau = 5,scale = 1):
 
     outDict = {}
 
-    outDict['updiag1'] = np.sum((np.absolute(xtp - xt) < 1*scale)) / N
-    outDict['updiag5'] = np.sum((np.absolute(xtp - xt) < 5*scale)) / N
+    outDict['updiag01'] = np.sum((np.absolute(xtp - xt) < 1*scale)) / N
+    outDict['updiag05'] = np.sum((np.absolute(xtp - xt) < 5*scale)) / N
 
-    outDict['downdiag1'] = np.sum((np.absolute(xtp + xt) < 1*scale)) / N
-    outDict['downdiag5'] = np.sum((np.absolute(xtp + xt) < 5*scale)) / N
+    outDict['downdiag01'] = np.sum((np.absolute(xtp + xt) < 1*scale)) / N
+    outDict['downdiag05'] = np.sum((np.absolute(xtp + xt) < 5*scale)) / N
 
-    outDict['ratdiag1'] = outDict['updiag1'] / outDict['downdiag1']
-    outDict['ratdiag5'] = outDict['updiag5'] / outDict['downdiag5']
+    outDict['ratdiag01'] = outDict['updiag01'] / outDict['downdiag01']
+    outDict['ratdiag05'] = outDict['updiag05'] / outDict['downdiag05']
 
-    outDict['parabup1'] = np.sum( ( np.absolute( xtp - np.square(xt) ) < 1 * scale ) ) / N
-    outDict['parabup5'] = np.sum( ( np.absolute( xtp - np.square(xt) ) < 5 * scale ) ) / N
+    outDict['parabup01'] = np.sum( ( np.absolute( xtp - np.square(xt) ) < 1 * scale ) ) / N
+    outDict['parabup05'] = np.sum( ( np.absolute( xtp - np.square(xt) ) < 5 * scale ) ) / N
 
-    outDict['parabdown1'] = np.sum( ( np.absolute( xtp + np.square(xt) ) < 1 * scale ) ) / N
-    outDict['parabdown5'] = np.sum( ( np.absolute( xtp + np.square(xt) ) < 5 * scale ) ) / N
+    outDict['parabdown01'] = np.sum( ( np.absolute( xtp + np.square(xt) ) < 1 * scale ) ) / N
+    outDict['parabdown05'] = np.sum( ( np.absolute( xtp + np.square(xt) ) < 5 * scale ) ) / N
+
+    outDict['parabup01_1'] = np.sum( ( np.absolute( xtp -( np.square(xt) + 1 ) ) < 1 * scale ) ) / N
+    outDict['parabup05_1'] = np.sum( ( np.absolute( xtp - (np.square(xt) + 1)) < 5 * scale ) ) / N
+
+    outDict['parabdown01_1'] = np.sum( ( np.absolute( xtp + np.square(xt) - 1 ) < 1 * scale ) ) / N
+    outDict['parabdown05_1'] = np.sum( ( np.absolute( xtp + np.square(xt) - 1) < 5 * scale ) ) / N
+
+    outDict['parabup01_n1'] = np.sum( ( np.absolute( xtp -( np.square(xt) - 1 ) ) < 1 * scale ) ) / N
+    outDict['parabup05_n1'] = np.sum( ( np.absolute( xtp - (np.square(xt) - 1)) < 5 * scale ) ) / N
+
+    outDict['parabdown01_n1'] = np.sum( ( np.absolute( xtp + np.square(xt) + 1 ) < 1 * scale ) ) / N
+    outDict['parabdown05_n1'] = np.sum( ( np.absolute( xtp + np.square(xt) + 1) < 5 * scale ) ) / N
+
+    outDict['ring1_01'] = np.sum( np.absolute( np.square(xtp) + np.square(xt) - 1 ) < 1 * scale ) / N
+    outDict['ring1_02'] = np.sum( np.absolute( np.square(xtp) + np.square(xt) - 1 ) < 2 * scale ) / N
+    outDict['ring1_05'] = np.sum( np.absolute( np.square(xtp) + np.square(xt) - 1 ) < 5 * scale ) / N
+
+    outDict['incircle_01'] = np.sum(  np.square(xtp) + np.square(xt)  < 1 * scale ) / N
+    outDict['incircle_02'] = np.sum( np.square(xtp) + np.square(xt)  < 2 * scale ) / N
+    outDict['incircle_05'] = np.sum(  np.square(xtp) + np.square(xt)  < 5 * scale ) / N
+
+
+    outDict['incircle_1'] = np.sum(  np.square(xtp) + np.square(xt)  < 10 * scale ) / N
+    outDict['incircle_2'] = np.sum( np.square(xtp) + np.square(xt)  < 20 * scale ) / N
+    outDict['incircle_3'] = np.sum(  np.square(xtp) + np.square(xt)  < 30 * scale ) / N
+
+    outDict['medianincircle'] = np.median([outDict['incircle_01'], outDict['incircle_02'], outDict['incircle_05'], \
+                            outDict['incircle_1'], outDict['incircle_2'], outDict['incircle_3']])
+
+    outDict['stdincircle'] = np.std([outDict['incircle_01'], outDict['incircle_02'], outDict['incircle_05'], \
+                            outDict['incircle_1'], outDict['incircle_2'], outDict['incircle_3']],ddof = 1)
 
 
     return outDict
