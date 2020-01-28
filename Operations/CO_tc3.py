b324b9788f854bf101e1978b39cb8cb101c093bf
jniestroy <jniestroy@gmail.com>
2020-01-28 10:06:59 -0500

    Add outputs to match matlab

--- Operations/CO_tc3.py
+++ Operations/CO_tc3.py
@@ -1,12 +1,31 @@
 def CO_tc3(y,tau = 'ac'):
+
     if tau == 'ac':
+
         tau = CO_FirstZero(y,'ac')
+
     else:
+
         tau = CO_FirstMin(y,'mi')
+
     N = len(y)
+
     yn = y[0:N-2*tau]
     yn1 = y[tau:N-tau]
     yn2 = y[tau*2:N]
+
     raw = np.mean(np.multiply(np.multiply(yn,yn1),yn2)) / (np.absolute(np.mean(np.multiply(yn,yn1))) ** (3/2))
 
-    return raw
+    outDict = {}
+
+    outDict['raw'] = raw
+
+    outDict['abs'] = np.absolute(raw)
+
+    outDict['num'] = np.mean(np.multiply(yn,np.multiply(yn1,yn2)))
+
+    outDict['absnum'] = np.absolute(outDict['num'])
+
+    outDict['denom'] = np.absolute( np.mean(np.multiply(yn,yn1)))**(3/2)
+
+    return outDict
--- Operations/CO_trev.py
+++ Operations/CO_trev.py
@@ -1,11 +1,30 @@
 def CO_trev(y,tau = 'ac'):
-        if tau == 'ac':
-            tau = CO_FirstZero(y,'ac')
-        else:
-            tau = CO_FirstMin(y,'mi')
-        N = len(y)
-        yn = y[0:N-tau]
-        yn1 = y[tau:N]
-        raw = np.mean(np.power(yn1-yn,3)) / np.mean(np.power(yn1 - yn,2))**(3/2)
 
-        return raw
+    if tau == 'ac':
+
+        tau = CO_FirstZero(y,'ac')
+
+    else:
+
+        tau = CO_FirstMin(y,'mi')
+
+    N = len(y)
+
+    yn = y[0:N-tau]
+    yn1 = y[tau:N]
+
+    raw = np.mean(np.power(yn1-yn,3)) / np.mean(np.power(yn1 - yn,2))**(3/2)
+
+    outDict = {}
+
+    outDict['raw'] = raw
+
+    outDict['abs'] = np.absolute(raw)
+
+    outDict['num'] = np.mean(np.power(yn1-yn,3))
+
+    outDict['absnum'] = np.absolute(outDict['num'])
+
+    outDict['denom'] = np.mean(np.power(yn1-yn,2))**(3/2)
+
+    return outDict
