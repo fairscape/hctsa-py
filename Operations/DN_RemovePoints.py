1d2cf1d4c66af3160b619065d0ecd1005ab5072c
jniestroy <jniestroy@gmail.com>
2020-02-06 09:58:12 -0500

    Updated to use Peasrson Kurtosis

--- Operations/DN_RemovePoints.py
+++ Operations/DN_RemovePoints.py
@@ -1,12 +1,19 @@
-def DN_RemovePoints(y,removeHow = 'absfar',p = .99):
+def DN_RemovePoints(y,removeHow = 'absfar',p = .1):
 
     if removeHow == 'absclose':
+
         i =  np.argsort(-np.absolute(y),kind = 'mergesort')
+
     elif removeHow == 'absfar':
+
         i = np.argsort(np.absolute(y),kind = 'mergesort')
+
     elif removeHow == 'min':
+
         i =  np.argsort(-y,kind = 'mergesort')
+
     elif removeHow == 'max':
+
         i = np.argsort(y,kind = 'mergesort')
 
     N = len(y)
@@ -24,17 +31,17 @@ def DN_RemovePoints(y,removeHow = 'absfar',p = .99):
 
     out['fzcacrat'] = CO_FirstZero(y_trim,'ac')/CO_FirstZero(y,'ac')
 
-    out['ac1rat'] = acf_y_trim[1]/acf_y[1]
+    out['ac1rat'] = acf_y_trim[0]/acf_y[0]
 
-    out['ac1diff'] = np.absolute(acf_y_trim[1]-acf_y[1])
+    out['ac1diff'] = np.absolute(acf_y_trim[0]-acf_y[0])
 
-    out['ac2rat'] = acf_y_trim[2]/acf_y[2]
+    out['ac2rat'] = acf_y_trim[1]/acf_y[1]
 
-    out['ac2diff'] = np.absolute(acf_y_trim[2]-acf_y[2])
+    out['ac2diff'] = np.absolute(acf_y_trim[1]-acf_y[1])
 
-    out['ac3rat'] = acf_y_trim[3]/acf_y[3]
+    out['ac3rat'] = acf_y_trim[2]/acf_y[2]
 
-    out['ac3diff'] = np.absolute(acf_y_trim[3]-acf_y[3])
+    out['ac3diff'] = np.absolute(acf_y_trim[2]-acf_y[2])
 
     out['sumabsacfdiff'] = sum(np.absolute(acf_y_trim-acf_y))
 
@@ -45,14 +52,19 @@ def DN_RemovePoints(y,removeHow = 'absfar',p = .99):
     out['std'] = np.std(y_trim)
 
     if stats.skew(y) != 0:
+
         out['skewnessrat'] = stats.skew(y_trim)/stats.skew(y)
 
-    out['kurtosisrat'] = stats.kurtosis(y_trim)/stats.kurtosis(y)
+    out['kurtosisrat'] = stats.kurtosis(y_trim,fisher=False)/stats.kurtosis(y,fisher=False)
 
     return out
 
 def SUB_acf(x,n):
+
     acf = np.zeros(n)
+
     for i in range(n):
-        acf[i] = CO_AutoCorr(x,i-1,'Fourier')
+
+        acf[i] = CO_AutoCorr(x,i,'Fourier')
+
     return acf
